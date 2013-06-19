e21eda4e3737c02b612e541b49d7304fc7ccaca3
teotigraphix <mschmalle@teotigraphix.com>
2013-06-19 17:04:29 -0400

    fixed storage root

--- CausticCore/src/main/java/com/teotigraphix/caustic/desktop/RuntimeUtils.java
+++ CausticCore/src/main/java/com/teotigraphix/caustic/desktop/RuntimeUtils.java
@@ -38,7 +38,7 @@ public class RuntimeUtils {
      * This has to be set at Application startup before ANYTHING happens
      * Android; Environment.getExternalStorageDirectory();
      */
-    public static String STORAGE_ROOT = "C:\\Users\\Work\\Documents";
+    public static String STORAGE_ROOT = null;
 
     /**
      * /mnt/sdcard/
