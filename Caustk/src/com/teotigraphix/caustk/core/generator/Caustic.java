2f18a7c95b495a1221ffe7aaeb543119c583227e
Michael Schmalle <mschmalle@teotigraphix.com>
2013-12-23 07:02:56 -0500

    moved sound generator back in

---
+++ Caustk/src/com/teotigraphix/caustk/core/generator/Caustic.java
@@ -0,0 +1,68 @@
+////////////////////////////////////////////////////////////////////////////////
+// Copyright 2012 Michael Schmalle - Teoti Graphix, LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0 
+// 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and 
+// limitations under the License
+// 
+// Author: Michael Schmalle, Principal Architect
+// mschmalle at teotigraphix dot com
+////////////////////////////////////////////////////////////////////////////////
+
+package com.teotigraphix.caustk.core.generator;
+
+import android.util.Log;
+
+import com.singlecellsoftware.causticcore.CausticCore;
+
+/**
+ * @author Michael Schmalle
+ * @copyright Teoti Graphix, LLC
+ * @since 1.0
+ */
+public class Caustic extends CausticCore {
+
+    private static final String TAG = "Caustic";
+
+    private boolean mDebug;
+
+    public boolean getDebug() {
+        return mDebug;
+    }
+
+    public void setDebug(boolean value) {
+        mDebug = value;
+    }
+
+    public Caustic() {
+        super();
+    }
+
+    @Override
+    public float SendOSCMessage(String msg) {
+        if (!mDebug) {
+            Log.d(TAG, msg);
+            return super.SendOSCMessage(msg);
+        } else {
+            System.out.println(msg);
+        }
+        return Float.NaN;
+    }
+
+    @Override
+    public String QueryOSC(String msg) {
+        if (!mDebug) {
+            return super.QueryOSC(msg);
+        } else {
+            return "";
+        }
+    }
+}
